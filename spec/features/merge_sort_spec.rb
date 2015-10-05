6c0511e3ec19ae8d38417f060cc1cff0c6b4ee09
Mary Dickson Diaz <mdickson@gmail.com>
2015-10-04 18:07:27 -0700

    add remainder line and fix merge tests

--- spec/features/merge_sort_spec.rb
+++ spec/features/merge_sort_spec.rb
@@ -4,11 +4,11 @@ require "merge_sort.rb"
 describe 'merge sort' do
   it "returns a valid result with strings" do
     names = ["Brian", "Anna", "Dorothy", "Cathy", "Mary", "Deb", "Lucy", "Zach", "Beatrice"]
-    assert_equal insertion_sort(names), ["Anna", "Beatrice", "Brian", "Cathy", "Deb", "Dorothy", "Lucy", "Mary", "Zach"]
+    assert_equal merge_sort(names), ["Anna", "Beatrice", "Brian", "Cathy", "Deb", "Dorothy", "Lucy", "Mary", "Zach"]
   end
 
   it "returns valid result with numbers" do
     numbers = [1,5,2,98,4,5,2]
-    assert_equal insertion_sort(numbers), [1,2,2,4,5,5,98]
+    assert_equal merge_sort(numbers), [1,2,2,4,5,5,98]
   end
 end
