14e0af29d543346058d85f343659f605f4977385
Mary Dickson Diaz <mdickson@gmail.com>
2015-10-10 15:38:53 -0700

    working ruby stack and tests

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,12 +1,41 @@
+Node = Struct.new(:value, :next)
+
 class Stack
-  def initialize(head=null)
-    @head = head
+  attr_accessor :head
+
+  def initialize()
+    @head = nil
   end
 
   def push(data)
+    if @head == nil
+      @head = Node.new(data, nil)
+    else
+      current = Node.new(data, @head)
+      @head = current
+    end
   end
 
   def pop
+    top = @head
+    @head = top.next
+    return top
   end
+
+  def to_s
+    string = ""
+    current = @head
+    until current.next.nil?
+      string += "#{current.value}, "
+      current = current.next
+    end
+    string += "#{current.value}"
+    return string
+  end
+
+  def peek
+    return @head.value
+  end
+
 end
 
--- spec/features/stack_spec.rb
+++ spec/features/stack_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-require "stack.rb"
+require "stack"
 
 describe 'stacks' do
   it "creates a valid node" do
@@ -23,7 +23,8 @@ describe 'stacks' do
     stack = Stack.new()
     stack.push(0)
     stack.push(12)
-    assert_equal stack.pop, 12
+    assert_equal stack.pop.value, 12
+    assert stack.head.value, 0
   end
 
   it "returns a string of values for to_s" do
@@ -31,6 +32,6 @@ describe 'stacks' do
     stack.push(0)
     stack.push(12)
     stack.push(13)
-    assert_equal stack.to_s, "0, 12, 13"
+    assert_equal stack.to_s, "13, 12, 0"
   end
 end
