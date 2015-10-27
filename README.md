b0e848a021fabddfd14cb42bf76397a8665c37e2
Mary Dickson Diaz <mdickson@gmail.com>
2015-10-27 15:32:56 -0700

    working hash and test and benchmark

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Description
 **Data Structures** a repo for data structures and algorithms. This collection holds sample code for a number of classic data structures implemented in Ruby, including:
 
-# Sort Methods:
+## Sort Methods:
 * insertion sort
 * merge sort
 * binary tree traversal (pre, post, and in-order)
@@ -14,6 +14,17 @@
 * stack
 * queue
 * binary tree
+* hash table (roll your own)
+
+# Hash table
+I implemented a hash table without using Ruby's built-in hash method. This data structure instanciates a hashtable with a specified number of slots (defaults to 1024) and accepts a key (as string) and value. It runs the key through a hash mechanism (convert string characters to ordinal value, concatenate, and modulo by number of slots) and stores the value in a node containing key, value, and "next" (if applicable) for future retrieval.
+
+TODOS:
+* should it accept (and rewrite) duplicate keys? or return an error?
+* can it be faster?
+* should it "stringify" non-string keys if possible, or return an error?
+* benchmark runs on test, can I set it to run on a specific rake command?
+* can I explore how my hash is diversifying (or not) the indexing? Would be interesting to see how it's divvying up the nodes and tinker with the hash algorithm for better/faster/more diverse results.
 
 ## Installation
 
--- spec/features/hash_spec.rb
+++ spec/features/hash_spec.rb
@@ -3,28 +3,38 @@ require "hash.rb"
 
 describe 'hash table' do
   it "creates a hash table of fixed size" do
-    hash = HashTable.new(1024)
+    hash = HashTable.new(3)
+    assert_equal [nil, nil, nil], hash.slots
   end
 
   it "hashes a key" do
-    assert_equal ("?????"), hash.hash("one")
+    hash = HashTable.new(1024)
+    assert_equal 981, hash.my_hash("one")
   end
 
   it "stores a value at a given key" do
-    assert_equal ("one", 1), hash.set("one", 1)
-    assert_equal ("ten", 10), hash.set("ten", 10)
-    assert_equal ("five", "bananas"), hash.set("five", "bananas")
+    hash = HashTable.new(1024)
+    hash.set("one", 1).class == HashNode
+    assert_equal "key must be a string", hash.set(1, 1)
+    assert_equal "key must be a string", hash.set(:one, 1)
   end
 
   it "gets a value stored at a given key" do
+    hash = HashTable.new(1024)
+    hash.set("one", 1)
+    hash.set("ten", 10)
+    hash.set("five", "bananas")
     assert_equal 1, hash.get("one")
     assert_equal 10, hash.get("ten")
     assert_equal "bananas", hash.get("five")
+    assert_equal false, hash.get("two")
   end
 
   it "deletes a value at a given key, and returns the value" do
+    hash = HashTable.new(1024)
+    hash.set("five", "bananas")
     assert_equal "bananas", hash.delete("five")
-    assert_equal nil, hash.get("five") # should not find it after deleted
+    assert_equal false, hash.get("five") # should not find it after deleted
   end
 end
 
